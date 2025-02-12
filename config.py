069dc3383d78c864fb92d518652ee42609f4424e
wasankob69 <wasan.kob@hotmail.com>
2025-02-12 16:38:34 +0700

    Update

---
+++ config.py
@@ -0,0 +1,4 @@
+# ตั้งค่าต่างๆ สำหรับบอท
+TOKEN = "7864474274:AAGfIlZXU6to65STSxrzo9VNiq1JhmAMHxU"  # Token ของบอท
+CHAT_ID = "6511955957"  # Chat ID ของคุณ
+API_URL = "https://goldtrade.onrender.com/get_signal"  # API เทรดที่ใช้ดึงข้อมูล
